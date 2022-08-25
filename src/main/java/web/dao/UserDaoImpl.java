a59e06336a66b76ac99745b1b824b7d802e3e4e2
Никита <nikita-vip1904@yandex.ru>
2022-08-26 00:19:21 +0300

    Works

---
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -0,0 +1,46 @@
+package web.dao;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import web.model.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class UserDaoImpl implements UserDao {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void addUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    public List<User> getListUser() {
+        return entityManager.createQuery("select u from User u", User.class).getResultList();
+    }
+
+    @Override
+    public void updateUserById(int id, User upUser) {
+        User anotherUser = findUserById(id);
+
+        anotherUser.setName(upUser.getName());
+        anotherUser.setSurname(upUser.getSurname());
+        anotherUser.setAge(upUser.getAge());
+        entityManager.merge(anotherUser);
+    }
+
+    @Override
+    public void removeUserById(int id) {
+        entityManager.remove(findUserById(id));
+    }
+
+    @Override
+    public User findUserById(int id) {
+        return entityManager.find(User.class, id);
+    }
+}
